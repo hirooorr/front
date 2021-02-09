df1362a20cc3e2a40b2b7887f130efd17915bea5
unknown <you@example.com git config --global user.email you@example.com>
2021-02-09 14:27:48 +0900

    /修正

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,8 +4,8 @@
     <div class="card">
         <p>ログイン</p>
         <div class="form">
-            <input placeholder="メールアドレス" type="email" v-model="email" />
-            <input placeholder="パスワード" type="password" v-model="password" />
+            <input placeholder="メールアドレス" type="email" v-model="email">
+            <input placeholder="パスワード" type="password" v-model="password">
             <button @click="auth">ログイン </button>
         </div>
     </div>
